84ba72997b68f29ae0b0909eac80873ba69d4775
Antonio <antoniojosedonishung@gmail.com>
2022-11-29 10:19:50 -0500

    Landing

---
+++ internal/immutable/chunks/1-8b21c153.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/error.svelte-54f0b63e.js";export{t as component};
---
+++ internal/immutable/chunks/2-c4a6ab6a.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-8855e997.js";export{t as component};
