4f2f8dfb60d7dee01e48de1943d90fdf633048fe
Antonio <antoniojosedonishung@gmail.com>
2023-02-17 17:19:46 -0500

    fix: added new portfolio

---
+++ internal/immutable/chunks/1-3fe7ea1f.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/error.svelte-521e6901.js";export{t as component};
---
+++ internal/immutable/chunks/2-5cda84ab.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-3587865f.js";export{t as component};
