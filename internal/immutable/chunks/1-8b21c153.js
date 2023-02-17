f1bcefc00bc67ae52961d9fdbf7e2396762f3fdd
Antonio <antoniojosedonishung@gmail.com>
2023-02-17 17:18:42 -0500

    fix: removed old portfolio

--- internal/immutable/chunks/1-8b21c153.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/error.svelte-54f0b63e.js";export{t as component};
--- internal/immutable/chunks/2-097d193e.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_page.svelte-e6ab8392.js";export{t as component};
