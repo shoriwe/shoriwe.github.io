3d195d96d4c3ff0b5dd5ea766649b0f38e03492a
Antonio <antoniojosedonishung@gmail.com>
2023-07-05 08:03:42 -0500

    fix: updated personal portfolio, improved performance

---
+++ internal/immutable/chunks/2-3247d028.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-92b27c7b.js";export{t as component};
--- internal/immutable/chunks/2-5cda84ab.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_page.svelte-3587865f.js";export{t as component};
